e39c2c577ea838157bba14cc8159884988548f6f
Irfan Muhammad Faisal <45427562+IrfanKnowledge@users.noreply.github.com>
2019-04-25 23:01:09 +0700

    2

--- src/Player.java
+++ src/Player.java
@@ -5,5 +5,7 @@ public class Player {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
     }
 }
