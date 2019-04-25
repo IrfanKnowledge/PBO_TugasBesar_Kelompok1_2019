64b1ba3e88a2f922c8053252f77ad76655ddede9
Irfan Muhammad Faisal <45427562+IrfanKnowledge@users.noreply.github.com>
2019-04-25 22:57:44 +0700

    1

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,9 @@
 public class Player {
+    int idPlayer;
+    double uang;
+
     public static void main(String[] args) {
+        System.out.println("test");
         System.out.println("test");
     }
 }
